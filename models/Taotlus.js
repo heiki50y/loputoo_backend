c81c249009fa6bb67ac9a1d26e1c07795f325419
heiki.kotsar@khk.ee <heiki5000@gmail.com>
2020-05-05 20:43:07 +0300

    taotlus controller and modal fix

--- models/Taotlus.js
+++ models/Taotlus.js
@@ -14,6 +14,10 @@ const TaotlusSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  oppegrupp: {
+    type: String,
+    required: true
+  },
   periood: {
     type: String,
     required: true
