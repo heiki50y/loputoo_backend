868024f3d157c5b10a0a73af5c83b2bb8fcf5101
heiki.kotsar@khk.ee <heiki5000@gmail.com>
2020-05-26 00:15:34 +0300

    testimine

---
+++ app.test.js
@@ -0,0 +1,35 @@
+const supertest = require('supertest');
+const app = require('./server');
+const mongoose = require('mongoose')
+const config = require('config');
+const db = config.get('mongoURI');
+
+describe("Testing the movies API", () => {
+    beforeAll(async () => {
+        await mongoose.connect(db, { useNewUrlParser: true })
+    })
+
+
+	it("Tests the base route", async (done) => {
+
+		const response = await supertest(app).get('/');
+
+        expect(response.status).toBe(200);
+
+        done()
+    });
+
+    it('Should save user to database', async (done) => {
+        let username = Math.random().toString(36).substring(7);
+
+        const response = await supertest(app).post('/api/auth/signup')
+          .send({
+            name: 'test.name',
+            email: 'testing@gmail.com',
+            password: 'qwerty',
+            group: 'vs'
+          })
+
+        done()
+    })
+});
--- server.js
+++ server.js
@@ -8,20 +8,19 @@ const cors = require('cors');
 const mongoSanitize = require('express-mongo-sanitize');
 const { protect, authorize } = require('./middleware/auth');
 const errorHandler = require('./middleware/errorHandler');
-
+const router = express.Router();
 const app = express();
 
 connectDB();
 
 
 // Init Middleware
-app.use(cors());
-// app.use(cors(
-//     {
-//         origin: 'http://localhost:8080',
-//         credentials: true
-//     }
-// ))
+app.use(cors(
+    {
+        origin: 'http://localhost:8080'
+        
+    }
+))
 
 app.use(express.json());
 app.use(cookieParser());
@@ -44,7 +43,7 @@ app.use('/taotlused', protect, authorize('admin'), express.static('praktikataotl
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/taotlus', require('./routes/taotlus'));
+// app.use('/api/taotlus', require('./routes/taotlus'));
 app.use('/api/company', require('./routes/company'));
 app.use('/api/documents', require('./routes/documents'));
 app.use('/api/pdf', require('./routes/pdf'))
@@ -56,8 +55,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => res.send('Lopputoo backend'))
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage')
+})
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = server;
