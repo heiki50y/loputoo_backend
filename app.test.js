03cae39c7f903f85b2a241cbffb0ff378f162099
heiki.kotsar@khk.ee <heiki5000@gmail.com>
2020-05-26 10:24:46 +0300

    testimis parandus

--- app.test.js
+++ app.test.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const config = require('config');
 const db = config.get('mongoURI');
 
-describe("Testing the loputoo API", () => {
+describe("IKT-osakonna praktika dokumentide haldussüsteemi testimine", () => {
     beforeAll(async () => {
         await mongoose.connect(db, { useNewUrlParser: true })
     })
